59e2ab2a076a7c55e52f4c425df912c06667bdb1
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-27 12:22:43 -0600

    installs client_side as an app in settings

--- adventure_day/settings.py
+++ adventure_day/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client_side',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'adventure_day.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["os.path.join(SETTINGS_PATH, 'templates')"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
