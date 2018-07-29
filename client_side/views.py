4d5ec87ea6d0a8d78891eac8d14d80616669485a
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-29 12:21:22 -0600

    Adds static folder, moves style.css around

--- client_side/views.py
+++ client_side/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template import loader
 
-def index(request):
-    return render(request, 'adventure_day/adventure.html')
+def home(request):
+    return render(request, 'adventure_day/home.html')
