70eb46c6057626db9980fb5b83babe349a98c0b8
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-29 14:44:15 -0600

    Changes access to secrets from os.environ to config utilizing python decouple

--- adventure_day/settings.py
+++ adventure_day/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
+FOURSQUARE_CLIENT_ID = config('FOURSQUARE_CLIENT_ID')
+FOURSQUARE_CLIENT_SECRET = config('FOURSQUARE_CLIENT_SECRET')
+GOOGLE_MAPS_PLATFORM_API_KEY = config('GOOGLE_MAPS_PLATFORM_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +83,7 @@ WSGI_APPLICATION = 'adventure_day.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=config('DATABASE_URL')
     ),
     # 'test': {
     #     'ENGINE': 'django.db.backends.postgresql',
