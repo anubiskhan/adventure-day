7c0e3f93f8c752140a5abacca2dc8f4ef761948a
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-29 19:48:55 -0600

    Adds methods for creating a route using custom origin

--- static/app.js
+++ static/app.js
@@ -1,17 +1,17 @@
-function codeAddress() {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({
-    'address': address
-  }, function (results, status) {
-    if (status == 'OK') {
-      return results[0].geometry.location;
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
+function createAdventure() {
+  var startAddressRaw = document.getElementById('start-address-field').value;
+  var startAddressFormatted = startAddressRaw.split(' ').join('+')
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${startAddressFormatted}&key=AIzaSyC_n7L6BbBnoCl6BxJcj3qSo_jurQLueCE`)
+                          .then(response => {
+                            return response.json();
+                          })
+                          .then(result => {
+                            geocodeAddress(result.results[0].geometry.location);
+                          });
 }
 
-function createAdventure() {
-  var startAddress = document.getElementById('start-address-field').value;
-  location.href = "https://www.google.com/maps/dir/?api=1&origin=39.8956,-104.9564&destination=39.7508,-104.9966&waypoints=periodic%20brewing|station26|fallingrock&travelmode=walking";
+function geocodeAddress(result) {
+  var origin = `${result.lat},${result.lng}`;
+  var destination = "39.7508,-104.9966"
+  location.href = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&waypoints=periodic%20brewing|station26|fallingrock&travelmode=walking`;
 }
